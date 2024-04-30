ac92ce56690a7e876eee31180d3a7d7f85c3d4fe
gopalrana <57245908+gopalrana@users.noreply.github.com>
2024-04-30 21:24:17 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 axios.defaults.withCredentials = true;
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('mern-m6ui2jad3-gopalrajputs-projects.vercel.app/register', {name, email, password})
+    axios.post('https://mern-flax.vercel.app/register', {name, email, password})
     .then(result => console.log(result))
     .catch(err => console.log(err))
   }
