7966e57dfa10e47810a2a09f285e4c15e52f856d
gopalrana <57245908+gopalrana@users.noreply.github.com>
2024-05-01 11:50:00 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["https://deploy-mern-frontend.vercel.app"],
+  origin: ["http://localhost:5173"],
   methods: ["POST", "GET"],
   credentials: true
 }));
